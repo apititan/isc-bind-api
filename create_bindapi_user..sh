241efe81838057bab56d3a43f340bf55f45520a0
Chinthaka Deshapriya <33759310+cdeshapriya@users.noreply.github.com>
2022-10-12 15:18:08 +0530

    Update create_bindapi_user..sh

--- create_bindapi_user..sh
+++ create_bindapi_user..sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo useradd -r -m -d /opt/bindapi bindapi -s /bin/bash
-sudo su - bindapi
+sudo useradd -r -m -d /opt/pydnsapi pydnsapi -s /bin/bash
+sudo su - pydnsapi
 mkdir venvapi
 python3 -m venv venvapi
 ls -al venvapi
