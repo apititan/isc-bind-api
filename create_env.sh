15807a543486a7470c974bad6c1b24c581c7fb87
Chinthaka Deshapriya <33759310+cdeshapriya@users.noreply.github.com>
2022-10-14 00:14:30 +0530

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -2,16 +2,14 @@ mkdir -p -m 700 .sec
 tsig-keygen pydnsapi-nsapi1  > ./.sec/pydnsapi-nsapi1.key
 TSIG_PASSWORD=$(grep secret .sec/pydnsapi-nsapi1.key |  cut -f2 -d \")
 pydns_api_pass=$(date | sha256sum | base64 | head -c 64; echo)
-echo "pydns_api-key,$pydns_api_pass" > ./apikeys.pass
+echo "pydns_api_key,$pydns_api_pass" > ./apikeys.pass
 chmod 600 apikeys.pass
 sudo cp ./.sec/pydnsapi-nsapi1.key /etc/bind/pydnsapi-nsapi1.conf
 
-echo "BIND_SERVER=172.25.0.20" > ./.env 
-echo "TSIG_USERNAME=pydnsapi-nsapi1" >> ./.env 
-echo TSIG_PASSWORD=${TSIG_PASSWORD} >> ./.env 
-echo "BIND_ALLOWED_ZONES=example.com,example.org" >> ./.env
+echo "BIND_SERVER=172.25.0.20" > ./.env
+echo "TSIG_USERNAME=pydnsapi-nsapi1" >> ./.env
+echo "TSIG_PASSWORD=${TSIG_PASSWORD}" >> ./.env
+echo "BIND_ALLOWED_ZONES=devops.apititan.works,hr.apititan.works" >> ./.env
 echo "API_KEY_FILE=apikeys.pass" >> ./.env
 echo "LOGGING_APPLICATION_NAME=pydnsapi-nsapi1" >> ./.env
 echo "LOGGING_DIR=./logs" >> ./.env
-
-                  
